c2d1097d7b0191f352dafa60f0c024f5f7364c94
Dejan Strbac <dejan.strbac@gmail.com>
2013-02-10 11:46:33 +0100

    update year

--- spa.js
+++ spa.js
@@ -14,7 +14,7 @@
  *        "#######      ###########"
  *          """"""       """""""""
  *
- *  Copyright (c) 2012, Dejan Strbac
+ *  Copyright (c) 2013, Dejan Strbac
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
